61c93660d7d9b6ca08144ee59897aa7f74d137d0
hninsatphyusin <hninsatphyusin99@gmail.com>
2025-08-11 21:27:38 +0800

    Added greetings

--- src/main/java/Peppa.java
+++ src/main/java/Peppa.java
@@ -1,11 +1,24 @@
 public class Peppa {
+    private static String LINE = "____________________________________________________________";
+    private static String NAME = "Peppa";
+
+    private static void greeting() {
+        String str = "Hello! I'm " + Peppa.NAME + "!\nWhat can I do for you?";
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
+        System.out.println(Peppa.LINE);
         String logo = ".______    _______ .______   .______      ___      \n"
                 + "|   _  \\  |   ____||   _  \\  |   _  \\    /   \\     \n"
                 + "|  |_)  | |  |__   |  |_)  | |  |_)  |  /  ^  \\    \n"
                 + "|   ___/  |   __|  |   ___/  |   ___/  /  /_\\  \\   \n"
                 + "|  |      |  |____ |  |      |  |     /  _____  \\  \n"
                 + "| _|      |_______|| _|      | _|    /__/     \\__\\\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println(Peppa.LINE);
+        greeting();
+        System.out.println(Peppa.LINE);
+
     }
 }
