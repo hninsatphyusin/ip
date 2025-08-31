d1939fdcdb1176649442486fff6778bf667483a6
hninsatphyusin <hninsatphyusin99@gmail.com>
2025-08-31 18:02:39 +0800

    Create Parser.java and shifted parsing from Peppa.java to Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,39 @@
+public class Parser {
+    private final TaskList tasks;
+    private final Storage storage;
+    private final Ui ui;
+
+    Parser(TaskList tasks, Storage storage, Ui ui) {
+        this.tasks = tasks;
+        this.storage = storage;
+        this.ui = ui;
+    }
+
+    public boolean parse(String input) { //returns false if quitting
+        input = input.trim(); //removes whitespace from both ends of a string
+        if (input.equals("bye")) {
+            return false;
+        } else if (input.equals("list")) {
+            tasks.displayTasks();
+        } else if (input.contains("unmark")) {
+            String[] arr = input.split(" ");
+            tasks.unmarkTask(Integer.valueOf(arr[1])-1);
+            storage.save(tasks);
+        } else if (input.contains("mark")) {
+            String[] arr = input.split(" ");
+            tasks.markTask(Integer.valueOf(arr[1])-1);
+            storage.save(tasks);
+        } else if (input.contains("delete")) {
+            String[] arr = input.split(" ");
+            tasks.deleteTask(Integer.valueOf(arr[1])-1);
+            storage.save(tasks);
+        } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
+            tasks.addTask(input);
+            storage.save(tasks);
+        } else {
+            System.out.println("Oopsies, I don't know what that means!");
+            ui.printline();
+        }
+        return true;
+    }
+}
--- src/main/java/Peppa.java
+++ src/main/java/Peppa.java
@@ -6,45 +6,26 @@ public class Peppa {
     private static boolean QUIT = false;
     private static Ui ui = new Ui();
     private static TaskList tasks;
+    private static Parser parser;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         Storage storage = new Storage("./data/Peppa.txt");
         try {
             tasks = new TaskList(storage.load(), ui);
         } catch (IOException e) {
             System.out.println(e);
         }
+        parser = new Parser(tasks, storage, ui);
         ui.printline();
         ui.printLogo();
         ui.greeting();
         ui.printline();
+
+        Scanner scanner = new Scanner(System.in);
         while (!QUIT) {
-            String command = scanner.nextLine();
-            if (command.equals("bye")) {
-                QUIT = true;
-                break;
-            } else if (command.equals("list")) {
-                tasks.displayTasks();
-            } else if (command.contains("unmark")) {
-                String[] arr = command.split(" ");
-                tasks.unmarkTask(Integer.valueOf(arr[1])-1);
-                storage.save(tasks);
-            } else if (command.contains("mark")) {
-                String[] arr = command.split(" ");
-                tasks.markTask(Integer.valueOf(arr[1])-1);
-                storage.save(tasks);
-            } else if (command.contains("delete")) {
-                String[] arr = command.split(" ");
-                tasks.deleteTask(Integer.valueOf(arr[1])-1);
-                storage.save(tasks);
-            } else if (command.contains("todo") || command.contains("deadline") || command.contains("event")) {
-                tasks.addTask(command);
-                storage.save(tasks);
-            } else {
-                System.out.println("Oopsies, I don't know what that means!");
-                ui.printline();
-            }
+            String next = scanner.nextLine();
+            QUIT = !parser.parse(next);
         }
 
         ui.exitMsg();
